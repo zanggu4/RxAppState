bb42b46b1d4cc8598dda95df39705da01a6244ae
Jörn Schoppe <joern@pixeldock.com>
2016-03-13 14:59:52 +0100

    Update example ViewController

--- Example/RxAppState/ViewController.swift
+++ Example/RxAppState/ViewController.swift
@@ -14,29 +14,28 @@ import RxAppState
 class ViewController: UIViewController {
     
     @IBOutlet weak var stateLabel: UILabel!
-    @IBOutlet weak var showAlertButton: UIButton!
+    @IBOutlet weak var firstLaunchLabel: UILabel!
+    @IBOutlet weak var appOpenedLabel: UILabel!
+    
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        showAlertButton.rx_tap
-            .subscribeNext {
-                // use this to trigger a system prompt that puts the app in an inactive state
-                let settings = UIUserNotificationSettings(forTypes: [.Alert, .Sound], categories: nil)
-                UIApplication.sharedApplication().registerUserNotificationSettings(settings)
-            }
+        let application = UIApplication.sharedApplication()
+        
+        application.rx_appState
+            .bindTo(stateLabel.rx_appState)
             .addDisposableTo(disposeBag)
         
-        let application = UIApplication.sharedApplication()
-        Observable.of(
-            application.rx_applicationDidBecomeActive,
-            application.rx_applicationWillResignActive,
-            application.rx_applicationDidEnterBackground,
-            application.rx_applicationWillTerminate
-            )
-            .merge()
-            .bindTo(stateLabel.rx_appState)
+        application.rx_firstLaunch
+            .bindTo(firstLaunchLabel.rx_firstLaunch)
+            .addDisposableTo(disposeBag)
+        
+        application.rx_didOpenAppCount
+            .subscribeNext { count in
+                self.appOpenedLabel.text = count == 1 ? "1 time" : "\(count) times"
+            }
             .addDisposableTo(disposeBag)
     }
 }
