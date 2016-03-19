8e1c3df494c1232dcbe9749b1588279eb094df3a
Jörn Schoppe <joern@pixeldock.com>
2016-03-19 20:55:08 +0100

    Add documentation

--- Pod/Classes/RxApplicationDelegateProxy.swift
+++ Pod/Classes/RxApplicationDelegateProxy.swift
@@ -11,16 +11,45 @@ import RxCocoa
 
 class RxApplicationDelegateProxy: DelegateProxy, UIApplicationDelegate, DelegateProxyType {
     
+    /**
+     For more information take a look at `DelegateProxyType`.
+     */
     static func currentDelegateFor(object: AnyObject) -> AnyObject? {
         let application: UIApplication = object as! UIApplication
         return application.delegate
     }
     
+    /**
+     For more information take a look at `DelegateProxyType`.
+     */
     static func setCurrentDelegate(delegate: AnyObject?, toObject object: AnyObject) {
         let application: UIApplication = object as! UIApplication
         application.delegate = delegate as? UIApplicationDelegate
     }
     
+    /**
+     This is an override to make sure that the original appDelegate is not deallocated
+     
+     Technically this creates a retain cycle. In this special case that is not a problem 
+     because UIApplication exists as long as the app exists anyway.
+     
+     It is necessary to retain the original AppDelegate because when RxApplicationDelegateProxy 
+     replaces the original delegate with the proxy it normally only keeps a weak reference 
+     to the original delegate to forward events to the original delegate.
+     
+     For other delegates this is the correct behaviour because other delegates usually are 
+     owned by another class (often a UIViewController). In case of the default app delegate
+     it is different because there is no class that owns it. When the application is initialized
+     the app delegate is explicitly initialized and allocated when UIApplicationMain() is called.
+     Because of this the first app delegate is released when another object is set as app delegate
+     
+     And that makes it necessary to retain the orignal app delegate when the proxy is set
+     as new delegate.
+     
+     Thanks to Michał Ciuba (https://twitter.com/MichalCiuba) who suggested this approach in
+     his answer to my question on Stack Overflow: 
+     http://stackoverflow.com/questions/35575305/transform-uiapplicationdelegate-methods-into-rxswift-observables
+     */
     override func setForwardToDelegate(delegate: AnyObject?, retainDelegate: Bool) {
         super.setForwardToDelegate(delegate, retainDelegate: true)
     }
--- Pod/Classes/UIApplication+Rx.swift
+++ Pod/Classes/UIApplication+Rx.swift
@@ -9,13 +9,39 @@
 import RxSwift
 import RxCocoa
 
+/**
+ UIApplication states
+ 
+ There are two more app states in the Apple Docs ("Not running" and "Suspended").
+ I decided to ignore those two states because there are no UIApplicationDelegate
+ methods for those states.
+ */
 public enum AppState: Equatable {
+    /**
+     The application is running in the foreground.
+     */
     case Active
+    /**
+     The application is running in the foreground but not receiving events.
+     Possible reasons:
+     - The user has opens Notification Center or Control Center
+     - The user receives a phone call
+     - A system prompt is shown (e.g. Request to allow Push Notifications)
+     */
     case Inactive
+    /**
+     The application is in the background because the user closed it.
+     */
     case Background
+    /**
+     The application is about to be terminated by the system
+     */
     case Terminated
 }
 
+/**
+ Equality function for AppState
+ */
 public func ==(lhs: AppState, rhs: AppState) -> Bool {
     switch (lhs, rhs) {
     case (.Active, .Active),
@@ -30,41 +56,68 @@ public func ==(lhs: AppState, rhs: AppState) -> Bool {
 
 extension UIApplication {
     
+    /**
+     Keys for NSUserDefaults
+     */
     private var firstLaunchKey:String { return "RxAppState_didLaunchBefore" }
     private var numDidOpenAppKey:String { return "RxAppState_numDidOpenApp" }
     
+    /**
+     Reactive wrapper for `delegate`.
+     
+     For more information take a look at `DelegateProxyType` protocol documentation.
+     */
     public var rx_delegate: DelegateProxy {
         return proxyForObject(RxApplicationDelegateProxy.self, self)
     }
     
+    /**
+     Reactive wrapper for `delegate` message `applicationDidBecomeActive(_:)`.
+     */
     public var rx_applicationDidBecomeActive: Observable<AppState> {
         return rx_delegate.observe(#selector(UIApplicationDelegate.applicationDidBecomeActive(_:)))
             .map { _ in
                 return .Active
-            }
+        }
     }
     
+    /**
+     Reactive wrapper for `delegate` message `applicationDidEnterBackground(_:)`.
+     */
     public var rx_applicationDidEnterBackground: Observable<AppState> {
         return rx_delegate.observe(#selector(UIApplicationDelegate.applicationDidEnterBackground(_:)))
             .map { _ in
                 return .Background
-            }
+        }
     }
     
+    /**
+     Reactive wrapper for `delegate` message `applicationWillResignActive(_:)`.
+     */
     public var rx_applicationWillResignActive: Observable<AppState> {
         return rx_delegate.observe(#selector(UIApplicationDelegate.applicationWillResignActive(_:)))
             .map { _ in
                 return .Inactive
-            }
+        }
     }
     
+    /**
+     Reactive wrapper for `delegate` message `applicationWillTerminate(_:)`.
+     */
     public var rx_applicationWillTerminate: Observable<AppState> {
         return rx_delegate.observe(#selector(UIApplicationDelegate.applicationWillTerminate(_:)))
             .map { _ in
                 return .Terminated
-            }
+        }
     }
     
+    /**
+     Observable sequence of the application's state
+     
+     This gives you an observable sequence of all possible application states.
+     
+     - returns: Observable sequence of AppStates
+     */
     public var rx_appState: Observable<AppState> {
         return Observable.of(
             rx_applicationDidBecomeActive,
@@ -75,6 +128,21 @@ extension UIApplication {
             .merge()
     }
     
+    /**
+     Observable sequence that emits a value whenever the user opens the app
+     
+     This is a handy sequence if you want to run some code everytime
+     the user opens the app.
+     It ignores `applicationDidBecomeActive(_:)` calls when the app was not
+     in the background but only in inactive state (because the user
+     opened control center or received a call).
+     
+     Typical use cases:
+     - Track when the user opens the app.
+     - Refresh data on app start
+     
+     - returns: Observable sequence of Void
+     */
     public var rx_didOpenApp: Observable<Void> {
         return Observable.of(
             rx_applicationDidBecomeActive,
@@ -85,9 +153,20 @@ extension UIApplication {
             .filter { $0 == .Active }
             .map { _ in
                 return
-            }
+        }
     }
     
+    /**
+     Observable sequence that emits the number of times a user has opened the app
+     
+     This is a handy sequence if you want to know how many times the user has opened your app
+     
+     Typical use cases:
+     - Ask a user to review your app after when he opens it for the 10th time
+     - Track the number of times a user has opened the app
+     
+     -returns: Observable sequence of Int
+     */
     public var rx_didOpenAppCount: Observable<Int> {
         return rx_didOpenApp
             .map { _ in
@@ -97,9 +176,20 @@ extension UIApplication {
                 userDefaults.setInteger(count, forKey: self.numDidOpenAppKey)
                 userDefaults.synchronize()
                 return count
-            }
+        }
     }
     
+    /**
+     Observable sequence that emits if the app is opened for the first time
+     
+     This is a handy sequence for all the times you want to run some code only
+     when the app is launched for the first time
+     
+     Typical use case:
+     - Show a tutorial to a new user
+     
+     -returns: Observable sequence of Bool
+     */
     public var rx_firstLaunch: Observable<Bool> {
         return rx_didOpenApp
             .map { _ in
@@ -113,6 +203,6 @@ extension UIApplication {
                     userDefaults.synchronize()
                     return true
                 }
-            }
+        }
     }
 }
