7d9f5192ddf5e89a0e8dcbca76e47121af8ed843
Jörn Schoppe <joern@pixeldock.com>
2016-10-09 18:23:16 +0200

    Add "isFirstLaunchOfNewVersion" to Example

--- Example/RxAppState/ViewController.swift
+++ Example/RxAppState/ViewController.swift
@@ -16,11 +16,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var stateLabel: UILabel!
     @IBOutlet weak var firstLaunchLabel: UILabel!
     @IBOutlet weak var appOpenedLabel: UILabel!
+    @IBOutlet weak var firstLaunchAfterUpdateLabel: UILabel!
+    @IBOutlet weak var appVersionLabel: UILabel!
+    @IBOutlet weak var simulateUpdateButton: UIButton!
     
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupExampleUI()
         
         let application = UIApplication.shared
         
@@ -46,6 +50,31 @@ class ViewController: UIViewController {
                 self.appOpenedLabel.text = count == 1 ? "1 time" : "\(count) times"
             })
             .addDisposableTo(disposeBag)
+        
+        /**
+         Show if the app is launched for the first time after an update
+         */
+        application.rx.isFirstLaunchOfNewVersion
+            .bindTo(firstLaunchAfterUpdateLabel.rx_firstLaunch)
+            .addDisposableTo(disposeBag)
+    }
+    
+    func setupExampleUI() {
+        appVersionLabel.text = RxAppState.currentAppVersion
+        
+        simulateUpdateButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.simulateAppUpdate()
+            })
+            .addDisposableTo(disposeBag)
+    }
+    
+    func simulateAppUpdate() {
+        guard let currentMinorVersion = RxAppState.currentAppVersion?.components(separatedBy: ".").last else { return }
+        let minorVersion = Int(currentMinorVersion) ?? 0
+        let newSimulatedAppVersion = "1.\(minorVersion + 1)"
+        RxAppState.currentAppVersion = newSimulatedAppVersion
+        appVersionLabel.text = newSimulatedAppVersion
     }
 }
 	
