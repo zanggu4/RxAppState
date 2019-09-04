f1dda9a38872c03bd0b13d29a20535eb1748c4ca
Sergey Kuleshov <svyatogor@gmail.com>
2019-09-04 13:20:07 +0300

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "RxSwiftExt",
+    platforms: [
+        .iOS(.v8), .tvOS(.v9), .macOS(.v10_11)
+    ],
+    products: [
+        .library(name: "RxAppState", targets: ["RxAppState"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+    ],
+    targets: [
+        .target(name: "RxAppState", dependencies: ["RxSwift", "RxCocoa"], path: "Source")
+    ],
+    swiftLanguageVersions: [.v4_2, .v5]
+)
